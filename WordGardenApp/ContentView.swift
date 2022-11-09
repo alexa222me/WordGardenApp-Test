bcd27e210900967dcf97f49e1f015cbf696455fe
Alexa Mendoza <alexandra96me@hotmail.com>
2022-11-08 22:33:12 -0600

    Initial Commit

---
+++ WordGardenApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WordGardenApp
+//
+//  Created by Alexa Mendoza on 11/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WordGardenApp/WordGardenAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenAppApp.swift
+//  WordGardenApp
+//
+//  Created by Alexa Mendoza on 11/8/22.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
